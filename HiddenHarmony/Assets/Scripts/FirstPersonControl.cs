a0969c8dbd5766f05f3c4a7c03fd56164f01f411
jswanso1 <jswanso1@ucsc.edu>
2019-01-15 02:30:25 -0800

    Added basic 3D player movement
    Basic 3D movement added. I followed a tutorial (I'm still only slightly familiar with Unity). I tried to comment as much as I could. The most documentation is needed for the camera controls. I see though I could implement controls without the player controller if necessary though. I'll probably work to refigure the controls a bit if there's more time.

---
+++ HiddenHarmony/Assets/Scripts/FirstPersonControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonControl : MonoBehaviour {
+    // Based on code from Stephen Barr
+
+    public enum RotationAxis{
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxis axes = RotationAxis.MouseX;
+
+    public float minVertical = -45.0f; //Minimum angle of vertical movement
+    public float maxVertical = 45.0f; //Maximum angle of vertical movement
+
+    public float sensHorizontal = 10.0f;
+    public float sensVertical = 10.0f;
+
+    public float rotationX = 0;
+
+    // Update is called once per frame
+    void Update () {
+        if (axes == RotationAxis.MouseX) {
+            transform.Rotate (0, Input.GetAxis ("Mouse X") * sensHorizontal, 0);
+        } else if (axes == RotationAxis.MouseY) {
+            rotationX -= Input.GetAxis ("Mouse Y") * sensVertical;
+            rotationX = Mathf.Clamp (rotationX, minVertical, maxVertical); //Clamps the vertical angle within the min and max limits (45 degrees)
+
+            float rotationY = transform.localEulerAngles.y;
+
+    		transform.localEulerAngles = new Vector3 (rotationX, rotationY, 0);
+        }
+    }
+}
---
+++ HiddenHarmony/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // Based on code from Stephen Barr
+    public float speed = 6f; //sets speed multiplier
+
+    private CharacterController character; //creates a game object for storage
+
+    // Use this for initialization
+    void Start () {
+        character = GetComponent<CharacterController> (); //gets the character controller from the GameObject
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        float moveX = Input.GetAxis ("Horizontal") * speed; //
+        float moveZ = Input.GetAxis ("Vertical") * speed;
+        Vector3 movement = new Vector3 (moveX, 0, moveZ);
+        
+        movement = Vector3.ClampMagnitude (movement, speed); //Limits the max speed of the player
+        movement *= Time.deltaTime; //Ensures the speed the player moves does not change based on frame rate
+        movement = transform.TransformDirection(movement);
+        character.Move (movement);
+    }
+}
