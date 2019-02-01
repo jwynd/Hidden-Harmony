ce4974122ece95910b394abcf6b6b228c99bdf39
jwynd <18224844+jwynd@users.noreply.github.com>
2019-02-01 14:49:23 -0800

    Sound Objects throws descriptive exceptions if bad values are entered by user

--- HiddenHarmony/Assets/SoundObject.cs
+++ HiddenHarmony/Assets/SoundObject.cs
@@ -26,6 +26,8 @@ public class SoundObject : MonoBehaviour
     // Start is called before the first frame update
     void Start(){
         aS = gameObject.GetComponent<AudioSource>();
+        if(offsetRange <= 0.0f) throw new System.ArgumentException("Offset Range must be greater than 0");
+        if(measureTime <= 0.0f) throw new System.ArgumentException("Measure Time must be greater than 0");
     }
 
     // Update is called once per frame
