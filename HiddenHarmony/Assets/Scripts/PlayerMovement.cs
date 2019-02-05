201022c274ea03a7bea915a262af6634d0f4a0bd
jswanso1 <38990292+jswanso1@users.noreply.github.com>
2019-02-05 07:38:02 -0800

    Jump works well
    Jump is now fine-tuned and able to be precisely adjusted. It also turns out there is a character controller velocity, but I realized after finishing

--- HiddenHarmony/Assets/Scripts/PlayerMovement.cs
+++ HiddenHarmony/Assets/Scripts/PlayerMovement.cs
@@ -4,54 +4,52 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     // Based on code from Stephen Barr
-    public float speed = 6f; //sets speed multiplier
-    public float jump = 65f;
 
+    private float yVelocity = 0f; //the counter to determine at which point in the jumpTime the player is at.
+    private bool jumping = false; //checks if the player is jumping
     private CharacterController character; //creates a game object for storage
-    private Rigidbody rigidbody;
+    //private Rigidbody rigidbody;
+
+    public float speed = 6f; //sets speed multiplier
+    //speed suggested value 6f
+    public float gravity = 7f; //the value subtracted from the y axis to calculate gravity
+    //gravity suggested value 7f. Actual value 9.8f
+    public float jumpTime = 17f; //the maximun value of the jumpTime
+    //jumpTime suggested value 17f;
+    public float jumpSpeed = 0.5f; //how fast the jumpTime accelerates per frame
+    //jumpSpeed suggested value 0.5f
+    public float canJump = 0.55f; //the distance between the player and the ground at which the player can jumpTime
+    //canJump suggested value 1.25f. Assumes player is of height 2 and takes half, which is 1. The 0.25 is added for wiggle room.
 
     // Use this for initialization
     void Start () {
         character = GetComponent<CharacterController> (); //gets the character controller from the GameObject
-        rigidbody = GetComponent<Rigidbody> ();
-    }
-    
+        //rigidbody = GetComponent<Rigidbody> ();
+    }    
+
     // Update is called once per frame
     void Update () {
-        /*
-        if(Input.GetKeyDown(KeyCode.Return)) {
-            character.enabled = !character.enabled;
-        }
-        */
-        /*if(this.transform.position.y < -10.0f){
-            this.transform.position = new Vector3(this.transform.position.x, 10.0f, this.transform.position.z);
-        }*/
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            //rigidbody.AddForce(new Vector3(0, jump, 0), ForceMode.Impulse);
-            Vector3 jumpMovement = new Vector3 (0, jump, 0);
-            
-            //jumpMovement = Vector3.ClampMagnitude (jumpMovement, speed); //Limits the max speed of the player
-            jumpMovement *= Time.deltaTime; //Ensures the speed the player moves does not change based on frame rate
-            jumpMovement = transform.TransformDirection(jumpMovement);
-            character.Move (jumpMovement);
+        if(Input.GetKeyDown(KeyCode.Space) && Physics.Raycast (transform.position, Vector3.down, canJump)) { // add && Physics.Raycast (transform.position, Vector3.down, canJump) for ground check?
+            if (!jumping){
+                jumping = true;            
+                yVelocity = jumpTime;
+            }
         }
 
-        // Manual gravity script because we needed something working.
-        if(this.transform.position.y > -100.0f){
-            character.Move(new Vector3(0.0f, -0.05f, 0.0f));
-        }
-        /*if(this.transform.position.y < 0.0f){
-            this.transform.position = new Vector3(this.transform.position.x, 0.0f, this.transform.position.z);
+        if(jumping && yVelocity > 0){
+            yVelocity -= jumpSpeed; //increases jumpTime velocity when jumping and before jumpTime total is reached
         }
         else{
-            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 5.0f*Time.deltaTime, this.transform.position.z);
-        }*/
+            jumping = false;
+            yVelocity = 0; //resets jumpTime velocity when jumpTime is finished
+        }
 
         float moveX = Input.GetAxis ("Horizontal") * speed; //
         float moveZ = Input.GetAxis ("Vertical") * speed;
         Vector3 movement = new Vector3 (moveX, 0, moveZ);
-        
+
         movement = Vector3.ClampMagnitude (movement, speed); //Limits the max speed of the player
+        movement.y = movement.y - gravity + yVelocity;
         movement *= Time.deltaTime; //Ensures the speed the player moves does not change based on frame rate
         movement = transform.TransformDirection(movement);
         character.Move (movement);
