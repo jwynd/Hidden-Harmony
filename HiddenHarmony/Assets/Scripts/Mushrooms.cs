1301d39b0ce023a6dba1952d90b0c2e2c6c52798
jwynd <18224844+jwynd@users.noreply.github.com>
2019-02-07 17:37:02 -0800

    Enable and disable mushrooms on keypress 'M'

---
+++ HiddenHarmony/Assets/Scripts/Mushrooms.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushrooms : MonoBehaviour
+{
+    private MeshRenderer[] mushrooms;
+    // Start is called before the first frame update
+    void Start(){
+        mushrooms = GetComponentsInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.M)){
+            foreach (MeshRenderer shroom in mushrooms){
+                shroom.enabled = !shroom.enabled;
+            }
+        }
+    }
+}
