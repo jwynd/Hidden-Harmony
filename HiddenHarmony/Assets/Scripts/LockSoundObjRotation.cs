1a417e792154135cbc5a741069105865f7b30314
jwynd <18224844+jwynd@users.noreply.github.com>
2019-02-08 18:17:43 -0800

    Added script which locks rotation of all objects tagged sound object and attatched to player prefab

---
+++ HiddenHarmony/Assets/Scripts/LockSoundObjRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockSoundObjRotation : MonoBehaviour
+{
+    private GameObject[] soundObjs;
+    // Start is called before the first frame update
+    void Start(){
+        soundObjs = GameObject.FindGameObjectsWithTag("SoundObj");
+        foreach(GameObject soundObj in soundObjs){
+            soundObj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+}
