df5713329c4f8c67620b27891d4e3ac4b8822123
jwynd <18224844+jwynd@users.noreply.github.com>
2019-02-10 14:35:46 -0800

    Fade v1 works on start

--- HiddenHarmony/Assets/Scripts/StartMenu.cs
+++ HiddenHarmony/Assets/Scripts/StartMenu.cs
@@ -8,8 +8,7 @@ public class StartMenu : MonoBehaviour
     private GameObject startMenuUI;
     private GameObject menuCamera;
     private GameObject mainCamera;
-    private GameObject fadePanel;
-    private Image fade;
+    private GameObject fade;
     private float fadeTime = 2.0f;
     private float timer = 0.0f;
     private bool fading = false;
@@ -19,25 +18,24 @@ public class StartMenu : MonoBehaviour
         startMenuUI = GameObject.Find("Canvas/StartMenuMain");
         menuCamera = GameObject.Find("MenuCamera");
         mainCamera = GameObject.Find("Player/MainCamera");
-        fadePanel = GameObject.Find("Canvas/FadePanel");
-        fade = fadePanel.GetComponent<Image>();
-        fadePanel.SetActive(false);
+        fade = GameObject.Find("MenuCamera/Fade");
     }
 
     void Update() {
         if(fading){
-            fadePanel.SetActive(true);
             
 
-            fade.CrossFadeAlpha(1, fadeTime, false);
+            fade.GetComponent<MeshRenderer>().material.color = 
+                Color.Lerp(fade.GetComponent<MeshRenderer>().material.color,
+                           Color.black, fadeTime * Time.deltaTime);
             timer += Time.deltaTime;
 
             if(timer > fadeTime){
-                // do this after everything has faded
-                fadePanel.SetActive(false);
+                // do this after everything has fade
                 mainCamera.GetComponent<Camera>().enabled = true;
                 mainCamera.GetComponent<FirstPersonControl>().enabled = true;
                 mainCamera.GetComponent<AudioListener>().enabled = true;
+                Destroy(fade);
                 Destroy(menuCamera);
                 fading = false;
             }
