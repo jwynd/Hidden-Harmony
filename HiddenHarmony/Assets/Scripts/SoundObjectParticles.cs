0f45eb4b91481b91c85cca84f2a4617a8784c94f
jswanso1 <38990292+jswanso1@users.noreply.github.com>
2019-02-15 09:01:11 -0800

    Stump-only particles
    Particles should only appear when being dropped on stage objects

--- HiddenHarmony/Assets/Scripts/SoundObjectParticles.cs
+++ HiddenHarmony/Assets/Scripts/SoundObjectParticles.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions; // used for Regex Matching
 
 public class SoundObjectParticles : MonoBehaviour
 {
     private ParticleSystem particle;
     private bool onGround;
+    private string pattern = "StageObj";
 
     public float dropDist = 0.55f;
     // Start is called before the first frame update
@@ -20,8 +22,12 @@ public class SoundObjectParticles : MonoBehaviour
     	RaycastHit hit;
         Ray stageRay = new Ray(transform.position, Vector3.down);
     	if(Physics.Raycast(stageRay, out hit, dropDist) && !onGround){
-    		particle.Play();
-    		onGround = true;
+    		Match match = Regex.Match(hit.collider.tag, pattern);
+            if(match.Success)
+            {
+    			particle.Play();
+    			onGround = true;
+    		}
     	}
         if(Physics.Raycast(stageRay, out hit, dropDist)){
         	onGround = true;
