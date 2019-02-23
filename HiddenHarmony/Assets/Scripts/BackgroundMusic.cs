38d82dfe3afc6feac594a98c06b3a635e3ecc3fa
jwynd <18224844+jwynd@users.noreply.github.com>
2019-02-23 14:31:45 -0800

    Fixed bugs in BackgroundMusic.cs

--- HiddenHarmony/Assets/Scripts/BackgroundMusic.cs
+++ HiddenHarmony/Assets/Scripts/BackgroundMusic.cs
@@ -6,24 +6,30 @@ public class BackgroundMusic : MonoBehaviour
 {
     [SerializeField] private int[] loopLengths;
     [SerializeField] private int[] BPMs;
-    private float[] measureTimes = new float[loopLengths.Length];
+    private int len;
+    private float[] measureTimes;
     private AudioSource[] bgs;
-    private float[] resetTimers = new float[measureTimes.Length];
+    private float[] resetTimers;
     private Timekeeper timekeeper;
     // Start is called before the first frame update
     void Start(){
+        if(loopLengths.Length == BPMs.Length) len = loopLengths.Length;
+        else throw new System.ArgumentException("Loop Lenghts have the same number of elements as BPMs");
+        measureTimes = new float[len];
+        resetTimers = new float[len];
         timekeeper = GameObject.Find("Timekeeper").GetComponent<Timekeeper>();
         bgs = gameObject.GetComponents<AudioSource>();
-        for(int i = 0; i < loopLengths; ++i){
+        if(bgs.Length != len) throw new System.ArgumentException("Must be as many loop lengths and BPMs as AudioSources");
+        for(int i = 0; i < loopLengths.Length; ++i){
             measureTimes[i] = (timekeeper.GetBeat(BPMs[i])*loopLengths[i]);
-            resetTimers = 0.0f;
+            resetTimers[i] = 0.0f;
         }
     }
 
     // Update is called once per frame
     void FixedUpdate(){
-        foreach(float timer in resetTimers){
-            timer += time.fixedDeltaTime;
+        for(int i = 0; i < resetTimers.Length; ++i){
+            resetTimers[i] += Time.fixedDeltaTime;
         }
         for(int i = 0; i < loopLengths.Length; ++i){
             if(resetTimers[i] > measureTimes[i]){
