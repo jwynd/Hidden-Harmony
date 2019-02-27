d6c0b10717bba2901373eb3b3e2f381612746270
jwynd <18224844+jwynd@users.noreply.github.com>
2019-02-27 13:24:01 -0800

    Added small script to draw gizmo at position of self

---
+++ HiddenHarmony/Assets/Scripts/EmptyGameObjectGizmo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyGameObjectGizmo : MonoBehaviour
+{
+    [SerializeField] private float gizmoRadius = 0.1f;
+    void OnDrawGizmos(){
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, gizmoRadius);
+    }
+}
