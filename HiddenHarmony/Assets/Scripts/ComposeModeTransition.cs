1686b9ea230ff91bbd434dd648535bb74766e5d2
jwynd <18224844+jwynd@users.noreply.github.com>
2019-03-07 14:12:10 -0800

    Fixed one compose mode bug and created another

--- HiddenHarmony/Assets/Scripts/ComposeModeTransition.cs
+++ HiddenHarmony/Assets/Scripts/ComposeModeTransition.cs
@@ -14,7 +14,8 @@ public class ComposeModeTransition : MonoBehaviour
     private float journeyLength;
     private bool transitioning = false;
     private bool compose = false;
-    private GameObject camera;
+    private GameObject playerCamera;
+    private GameObject composeCamera;
     private GameObject player;
 
 
@@ -27,7 +28,12 @@ public class ComposeModeTransition : MonoBehaviour
     // Start is called before the first frame update
     void Awake(){
         player = GameObject.Find("Player");
-        camera = GameObject.Find("Player/MainCamera");
+        playerCamera = GameObject.Find("Player/MainCamera");
+        composeCamera = new GameObject("composeCamera");
+        composeCamera.tag = "MainCamera";
+        composeCamera.transform.SetAsLastSibling();
+        composeCamera.AddComponent<Camera>();
+        composeCamera.SetActive(false);
     }
 
     // Update is called once per frame
@@ -40,9 +46,9 @@ public class ComposeModeTransition : MonoBehaviour
                 player.GetComponent<PlayerMovement>().enabled = false;
                 player.GetComponent<FirstPersonControl>().enabled = false;
                 player.GetComponent<Pickup>().enabled = false;
-                camera.GetComponent<FirstPersonControl>().enabled = false;
-                camera.transform.SetParent(null);
-                cameraOrigin = camera.transform.position;
+                playerCamera.GetComponent<FirstPersonControl>().enabled = false;
+                playerCamera.transform.SetParent(null);
+                cameraOrigin = playerCamera.transform.position;
                 compose = true;
                 transitioning = true;
                 startTime = Time.time;
@@ -54,8 +60,10 @@ public class ComposeModeTransition : MonoBehaviour
                 player.GetComponent<PlayerMovement>().enabled = true;
                 player.GetComponent<FirstPersonControl>().enabled = true;
                 player.GetComponent<Pickup>().enabled = true;
-                camera.GetComponent<FirstPersonControl>().enabled = true;
-                
+                playerCamera.GetComponent<FirstPersonControl>().enabled = true;
+                composeCamera.transform.SetAsLastSibling();
+                composeCamera.SetActive(false);
+                playerCamera.SetActive(true);
                 compose = false;
                 transitioning = true;
                 startTime = Time.time;
@@ -63,32 +71,37 @@ public class ComposeModeTransition : MonoBehaviour
             }
         }
         if(compose && transitioning){
-            camera.transform.LookAt(cameraTarget);
+            playerCamera.transform.LookAt(cameraTarget);
 
             distCovered = (Time.time - startTime) * transitionSpeed;
 
             fracJourney = distCovered / journeyLength;
 
-            camera.transform.position = Vector3.Lerp(cameraOrigin, composeCameraPosition.position, fracJourney);
-            if(Vector3.Distance(camera.transform.position, composeCameraPosition.position) < 0.01f){
-                camera.transform.position = composeCameraPosition.position;
-                camera.transform.LookAt(cameraTarget);
+            playerCamera.transform.position = Vector3.Lerp(cameraOrigin, composeCameraPosition.position, fracJourney);
+            if(Vector3.Distance(playerCamera.transform.position, composeCameraPosition.position) < 0.01f){
+                playerCamera.transform.position = composeCameraPosition.position;
+                playerCamera.transform.LookAt(cameraTarget);
+                composeCamera.transform.position = composeCameraPosition.position;
+                composeCamera.transform.LookAt(cameraTarget);
+                composeCamera.transform.SetAsFirstSibling();
+                playerCamera.SetActive(false);
+                composeCamera.SetActive(true);
                 transitioning = false;
             }
         } else if (!compose && transitioning){
-            camera.transform.LookAt(cameraTarget);
+            playerCamera.transform.LookAt(cameraTarget);
 
             distCovered = (Time.time - startTime) * transitionSpeed;
 
             fracJourney = distCovered / journeyLength;
 
-            camera.transform.position = Vector3.Lerp(composeCameraPosition.position, cameraOrigin, fracJourney);
-            if(Vector3.Distance(camera.transform.position, cameraOrigin) < 0.01f){
-                camera.transform.position = cameraOrigin;
-                camera.transform.LookAt(cameraTarget);
+            playerCamera.transform.position = Vector3.Lerp(composeCameraPosition.position, cameraOrigin, fracJourney);
+            if(Vector3.Distance(playerCamera.transform.position, cameraOrigin) < 0.01f){
+                playerCamera.transform.position = cameraOrigin;
+                playerCamera.transform.LookAt(cameraTarget);
                 player.transform.LookAt(cameraTarget);
                 transitioning = false;
-                camera.transform.SetParent(player.transform);
+                playerCamera.transform.SetParent(player.transform);
             }
             Cursor.visible = false;
         }
