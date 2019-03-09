2124ad9c6332ef67e48707b506d2797d8e2350c4
jswanso1 <38990292+jswanso1@users.noreply.github.com>
2019-03-09 14:31:52 -0800

    Inventory System Added
    I added an inventory system to a test scene

---
+++ HiddenHarmony/Assets/Scripts/SoundObjectCompose.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+
+public class SoundObjectCompose : MonoBehaviour{
+    public GameObject itemDrop;
+    private string pattern = "StageObj";
+    private float checkDistance = 1000;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        RaycastHit hit;
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.Log("general test");
+        if(Physics.Raycast(mouseRay, out hit, checkDistance)){
+            //Debug.Log("hit");
+            Match match = Regex.Match(hit.collider.tag, pattern);
+            if(match.Success) {
+                //Debug.Log("match success");
+                if (Input.GetMouseButtonDown(0) && itemDrop != null) {
+                    GameObject placedObject;
+                    Transform hitObjectTransform = hit.collider.gameObject.transform;
+                    placedObject = Instantiate(itemDrop, new Vector3(hitObjectTransform.position.x, hitObjectTransform.position.y + 2, hitObjectTransform.position.z), hitObjectTransform.rotation);
+
+                }
+            }
+        }
+    }
+
+    public void setSoundObject(GameObject soundObject){
+        this.itemDrop = soundObject;
+    }
+
+}
