5666811797438458343e6f70b20646b8e152faeb
jwynd <18224844+jwynd@users.noreply.github.com>
2019-04-06 15:32:28 -0700

    added inverse triggers

---
+++ HiddenHarmony/Assets/Scripts/Progression/DestroyOnce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnce : MonoBehaviour
+{
+    [SerializeField] private GameObject[] deleteObjects;
+
+    private bool eq = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        eq = this.GetComponent<SoundObject>().onStage;
+        if(eq){
+            // print("Creating subWooferObjects");
+            foreach(GameObject obj in deleteObjects){
+                if(obj.activeSelf) obj.SetActive(false);
+            }
+        }
+    }
+}
---
+++ HiddenHarmony/Assets/Scripts/Progression/DestroyWhile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhile : MonoBehaviour
+{
+    [SerializeField] private GameObject[] deleteObjects;
+//    [SerializeField] private GameObject[] soundObjects;
+    private bool invisible = false;
+    // Start is called before the first frame update
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        invisible = !this.GetComponent<SoundObject>().onStage;
+//        foreach(GameObject obj in soundObjects){
+//            invisible = obj.GetComponent<SoundObject>().onStage;
+//            // print(obj.name+" "+eq);
+//            if(!invisible) break;
+//        }
+
+        foreach(GameObject obj in deleteObjects){
+            obj.SetActive(invisible);
+        }
+    }
+}
