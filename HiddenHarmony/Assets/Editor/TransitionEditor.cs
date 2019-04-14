8bae39fb01043dbb89a3488c2eff25f92be00538
jwynd <18224844+jwynd@users.noreply.github.com>
2019-04-13 18:39:17 -0700

    added transition script

---
+++ HiddenHarmony/Assets/Editor/TransitionEditor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Transition))]
+//[CanEditMultipleObjects]
+public class TransitionEditor : Editor {
+    SerializedProperty sourceProp;
+    SerializedProperty transitionTimeProp;
+    SerializedProperty skyboxProp;
+
+    void OnEnable(){
+        sourceProp = serializedObject.FindProperty("source");
+        transitionTimeProp = serializedObject.FindProperty("transitionTime");
+        skyboxProp = serializedObject.FindProperty("skybox");
+    }
+
+    public override void OnInspectorGUI(){
+
+        GUILayout.Label("Target Values\n");
+//        GUILayout.BeginHorizontal();
+
+        EditorGUILayout.PropertyField(sourceProp, new GUIContent("Audio Clip"), GUILayout.MaxWidth(300));
+        EditorGUILayout.PropertyField(skyboxProp, new GUIContent("Skybox"), GUILayout.MaxWidth(300));
+
+  //      GUILayout.EndHorizontal();
+
+/*        GUILayout.Label("Side 2\n");
+
+        GUILayout.BeginHorizontal();
+
+            EditorGUILayout.PropertyField(source2prop, new GUIContent("Audio Clip"));
+            EditorGUILayout.PropertyField(skybox1prop, new GUIContent("Skybox"));
+
+        GUILayout.EndHorizontal();*/
+
+        GUILayout.Label("\n");
+
+        EditorGUILayout.PropertyField(transitionTimeProp, new GUIContent("Transition Time"));
+
+        serializedObject.ApplyModifiedProperties();
+
+
+
+        // base.OnInspectorGUI();
+        /*GUILayout.Label("Side 1");
+        Transition t = (Transition)target;
+        
+        GUILayout.BeginHorizontal();
+
+            t.source1 = EditorGUILayout.ObjectField("AudioSource 1", t.source1, typeof(AudioSource), false);
+            t.skybox1 = EditorGUILayout.ObjectField("Skybox 1", t.skybox1, typeof(Texture), false);
+
+        GUILayout.EndHorizontal();*/
+
+
+    }
+}
---
+++ HiddenHarmony/Assets/Scripts/Transition.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public AudioClip source;
+    public float transitionTime;
+    public Material skybox;
+
+    private AudioSource[] bg;
+    private float timer = 0.0f;
+    private bool fading = false;
+    private AudioSource newBG;
+    private AudioSource oldBG;
+
+    // Start is called before the first frame update
+    void Start(){
+        bg = GameObject.Find("BackgroundMusic").GetComponents<AudioSource>();
+        print(source.ToString());
+//        samples = new float[source.samples * source.channels];
+//        source.GetData(samples, 0);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(fading){
+            // Lerp Skybox
+            timer += Time.deltaTime;
+            RenderSettings.skybox.Lerp(RenderSettings.skybox, skybox, timer/transitionTime);
+
+            //crossfade music
+            oldBG.volume -= Time.deltaTime/transitionTime;
+            newBG.volume += Time.deltaTime/transitionTime;
+
+            if(oldBG.volume < 0.05f){
+                oldBG.volume = 0.0f;
+                newBG.volume = 1.0f;
+                RenderSettings.skybox = skybox;
+                fading = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.name != "Player") return;
+        if(bg[0].volume > 0.0f){
+            oldBG = bg[0];
+            newBG = bg[1];
+        } else {
+            oldBG = bg[1];
+            newBG = bg[0];
+        }
+        if(oldBG.clip == source){print("Same Clip");return;}
+        newBG.volume = 0.0f;
+        newBG.clip = source;
+        newBG.Play();
+        fading = true;
+        timer = 0.0f;
+
+    }
+}
