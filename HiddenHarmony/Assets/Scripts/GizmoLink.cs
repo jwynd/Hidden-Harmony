86c0dfc35f1cdff6879392d38d09a430ec102938
jwynd <18224844+jwynd@users.noreply.github.com>
2019-05-04 19:07:18 -0700

    Gizmo link script

---
+++ HiddenHarmony/Assets/Scripts/GizmoLink.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This file draws a line from transform 1 to transform 2
+
+public class GizmoLink : MonoBehaviour
+{
+    [SerializeField] private Color gizmoColor = Color.blue;
+    [SerializeField] private Transform point1;
+    [SerializeField] private Transform point2;
+
+    void OnDrawGizmos(){
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawLine(point1.position, point2.position);
+    }
+}
