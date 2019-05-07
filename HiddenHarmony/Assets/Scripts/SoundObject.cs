fea4aa408e93f066c98cd0a1c57f49fa1f743973
jwynd <18224844+jwynd@users.noreply.github.com>
2019-05-07 14:47:37 -0700

    Remember to put functions inside of classes

--- HiddenHarmony/Assets/Scripts/SoundObject.cs
+++ HiddenHarmony/Assets/Scripts/SoundObject.cs
@@ -169,9 +169,9 @@ public class SoundObject : MonoBehaviour
         }
 */
     }
+    private int abs(int x){
+        if(x < 0) return x * -1;
+        else return x;
+    }
 }
 
-private int abs(int x){
-    if(x < 0) return x * -1;
-    else return x;
-}
