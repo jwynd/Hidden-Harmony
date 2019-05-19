219c22e41affc77ebcac8e7d30190f4fe3f4ef4b
Fen <k2lsurvivor@gmail.com>
2019-05-18 23:53:35 -0700

    Added geyser prefab, tweaked player movement/prefab

---
+++ HiddenHarmony/Assets/Scripts/GeyserLaunch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeyserLaunch : MonoBehaviour
+{
+    public float speed = 5f;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    void OnTriggerStay(Collider collide){
+        TravelUp(speed, collide);
+
+    }
+    void OnTriggerExit(Collider collide){
+        TravelUp(speed * 1.5f, collide);
+
+    }
+
+    void TravelUp(float travelSpeed , Collider collide){
+        if(collide.gameObject.name == "Player"){
+            movement.y = travelSpeed *10;
+            collide.gameObject.GetComponent<CharacterController> ().Move(movement);
+            collide.gameObject.GetComponent<PlayerMovement> ().yVelocity = collide.gameObject.GetComponent<PlayerMovement> ().terminalVelocity;
+        }
+    }
+}
