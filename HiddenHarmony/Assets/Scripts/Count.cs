b7259e4061c7ee75bb99b8313d2ede8504078820
jwynd <18224844+jwynd@users.noreply.github.com>
2019-05-21 16:37:28 -0700

    Count now also counts hub sound objects

--- HiddenHarmony/Assets/Scripts/Count.cs
+++ HiddenHarmony/Assets/Scripts/Count.cs
@@ -8,18 +8,22 @@ public class Count : MonoBehaviour
     [SerializeField] private GameObject den;
     [SerializeField] private GameObject forest;
     [SerializeField] private GameObject cavern;
+    [SerializeField] private GameObject hub;
 
     private string[] denChildren;
     private string[] forestChildren;
     private string[] cavernChildren;
+    private string[] hubChildren;
 
     private string[] denNames;
     private string[] forestNames;
     private string[] cavernNames;
+    private string[] hubNames;
 
     private int denCount = 0;
     private int forestCount = 0;
     private int cavernCount = 0;
+    private int hubCount = 0;
 
     void Awake(){
         denChildren = new string[den.transform.childCount];
@@ -34,21 +38,26 @@ public class Count : MonoBehaviour
         for(int i = 0; i < cavern.transform.childCount; i++){
             cavernChildren[i] = cavern.transform.GetChild(i).gameObject.name+"(Clone)";
         }
+        hubChildren = new string[hub.transform.childCount];
+        for(int i = 0; i < hub.transform.childCount; i++){
+            hubChildren[i] = hub.transform.GetChild(i).gameObject.name+"(Clone)";
+        }
         denNames = new string[denChildren.Length];
         forestNames = new string[forestChildren.Length];
         cavernNames = new string[cavernChildren.Length];
+        hubNames = new string[hubChildren.Length];
     }
 
+    #if UNITY_ENGINE
     void Update(){
         if(Input.GetKeyDown(KeyCode.C)){
-            foreach(string o in denChildren){
-                print(o);
-            }
             print("Den Count: "+denCount);
             print("Forest Count: "+forestCount);
             print("Cavern Count: "+cavernCount);
+            print("Hub Count: "+hubCount);
         }
     }
+    #endif
 
     public void IncrementCount(string n){
 
@@ -61,10 +70,12 @@ public class Count : MonoBehaviour
         foreach(string name in cavernNames){
             if(Equals(n, name)) return;
         }
+        foreach(string name in hubNames){
+            if(Equals(n, name)) return;
+        }
         foreach(string o in denChildren){
             if(n == o){
                 denNames[denCount++] = n;
-                print("TEST SUCCUESS");
                 return;
             }
         }
@@ -80,6 +91,12 @@ public class Count : MonoBehaviour
                 return;
             }
         }
+        foreach(string o in hubChildren){
+            if(Equals(n, o)){
+                hubNames[hubCount++] = n;
+                return;
+            }
+        }
         throw new System.ArgumentException("incrementCount failed to return");
     }
 
@@ -91,6 +108,9 @@ public class Count : MonoBehaviour
     }
     public int CavernCount(){
         return cavernCount;
+    }
+    public int HubCount(){
+        return hubCount;
     }
 
 }
