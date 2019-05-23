84b906b7a3b95cee08d2d03027186b94246e7e72
MatthewNutt <matthewnutt1996@gmail.com>
2019-05-23 13:18:51 -0700

    Added transition for going into orcastra zone

---
+++ HiddenHarmony/Assets/Scripts/OrcaTransTemp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrcaTransTemp : MonoBehaviour
+{
+    [SerializeField] private bool underWater = false;
+    public GameObject orcastra;
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "OrcaTrans")
+        {
+            Debug.Log("Colliding!");
+            if (other.gameObject.CompareTag("Switch"))
+            {
+                orcastra.SetActive(true);
+                Debug.Log("True!");
+            }
+            else
+            {
+                orcastra.SetActive(false);
+                Debug.Log("False!");
+            }
+        }
+    }
+}
