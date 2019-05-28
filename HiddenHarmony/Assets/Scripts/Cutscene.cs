bfa71abe8bb7bc4d11c60b343acac6e10237510e
jwynd <18224844+jwynd@users.noreply.github.com>
2019-05-28 12:01:44 -0700

    cutscene first pass

--- HiddenHarmony/Assets/Scripts/Cutscene.cs
+++ HiddenHarmony/Assets/Scripts/Cutscene.cs
@@ -1,77 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PathCreation;
-
+using UnityEngine.Video;
 
 public class Cutscene : MonoBehaviour
 {
-    [Tooltip("These are the points that the camera will look at, it will switch to the next point after the time in delay has pased")]
-    [SerializeField] private Transform[] lookAt;
-    [Tooltip("The camera will wait this long before looking at the next target as specified by \"Look At\"")]
-    [SerializeField] private float[] delays;
-    [Tooltip("These will be created at the delay minus the \"Appearance Offset\"")]
+    [Tooltip("Place the mp4 file for the cutscene here")]
+    [SerializeField] private VideoClip cutscene;
+    [Tooltip("The distance at which the cutscene can be started")]
+    [SerializeField] private float interactDistance = 10.0f;
+    [Tooltip("All game objects in the array will be enabled during the cutscene")]
     [SerializeField] private GameObject[] toEnable;
-    [Tooltip("Game Objects are enabled at delay minus this")]
-    [SerializeField] private float appearanceOffset;
-    [Tooltip("This is the Path Creator for the path that you want the camera to follow")]
-    [SerializeField] private PathCreator pathCreator;
 
-    private GameObject mainCamera;
-    private int index = 0;
-    private float timer = 0;
-    private bool runSequence = true;
+    private GameObject player;
+    private GameObject camera;
+    private GameObject intMsg;
+    private VideoPlayer vp;
+    private bool prepared;
+    private bool played;
 
-    void OnValidate(){
-        if(appearanceOffset < 0.0f) appearanceOffset = 0.0f;
+    void Awake(){
+        player = GameObject.Find("Player");
+        camera = GameObject.Find("Player/MainCamera");
+        intMsg = GameObject.Find("InteractMessageController");
     }
 
-
-    void Awake()
+    void Start()
     {
-        if(lookAt.Length != delays.Length || delays.Length != toEnable.Length){
-            #if UNITY_EDITOR
-            throw new System.ArgumentException("All arrays must be of identical length");
-            UnityEditor.EditorApplication.isPlaying = false;
-            #else
-            Application.Quit();
-            #endif
-        }
-    }
-
-    void Start(){
-        mainCamera = GameObject.Find("MainCamera");
-        //mainCamera.GetComponent<PathFollower>().enabled = true;
-        Fadeout(Color.black); //Fadeout camera
-        // Camera will snap to position when the pathCreator is enabled
-        pathCreator.enabled = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(runSequence){// if the sequence has not run to completion
-            
-            if(index < lookAt.Length){
-                //Main Portion
-                mainCamera.transform.LookAt(lookAt[index]);
-                if(timer > delays[index] - appearanceOffset){
-                    Fadeout(Color.white); // this should happen a certain number of seconds before the object appears and only once
-                    toEnable[index].SetActive(true);
+        RaycastHit hit;
+        Ray cutRay = new Ray(camera.transform.position, camera.transform.forward);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(cutRay, out hit, interactDistance))
+            {
+                if(hit.collider.gameObject == this.gameObject)
+                {
+                    player.GetComponent<CharacterController>().enabled = false;
+                    player.GetComponent<PlayerMovement>().enabled = false;
+                    player.GetComponent<FirstPersonControl>().enabled = false;
+                    player.GetComponent<InventoryAdd>().enabled = false;
+                    player.GetComponent<InteractScript>().enabled = false;
+                    camera.GetComponent<FirstPersonControl>().enabled = false;
+                    camera.GetComponent<Camera>().far = 0.31f;
+                    vp = camera.AddComponent<VideoPlayer>();
+                    vp.clip = cutscene;
+                    vp.renderMode = VideoRenderMode.CameraNearPlane;
+                    vp.playOnAwake = false;
+                    vp.targetCameraAlpha = 1.0f;
+                    vp.isLooping = false;
+                    vp.Play();
+                    played = true;
+                    foreach(GameObject o in toEnable){
+                        o.SetActive(true);
+                    }
                 }
-            } else {
-                runSequence = false;
-                Destroy(pathCreator);
-                GameObject.Find("CameraChange").GetComponent<ComposeModeTransition>().composeImmediate();
             }
-            timer += Time.deltaTime;// Increment the time
-            if(timer > delays[index]) index++;
+        } 
+        if(!prepared && vp != null) prepared = vp.isPrepared;
+        if(played && prepared && !vp.isPlaying){
+            player.GetComponent<CharacterController>().enabled = true;
+            player.GetComponent<PlayerMovement>().enabled = true;
+            player.GetComponent<FirstPersonControl>().enabled = true;
+            player.GetComponent<InventoryAdd>().enabled = true;
+            player.GetComponent<InteractScript>().enabled = true;
+            camera.GetComponent<FirstPersonControl>().enabled = true;
+            camera.GetComponent<Camera>().far = 1000.0f;
+            Destroy(vp);
+            Destroy(this.gameObject);
         }
-        // Destroy(pathCreator); //This runs at the end of the script
-    }
-
-    private void Fadeout(Color fadeColor){
-        // Add fadeout as a post processing effect
-        throw new System.ArgumentException("Fadeout not written");
     }
 }
