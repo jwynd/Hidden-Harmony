df7c4e0b191b90828dcdfc0ce8de66c7d46a90c4
jwynd <18224844+jwynd@users.noreply.github.com>
2019-06-04 17:55:52 -0700

    Added end of game trigger

---
+++ HiddenHarmony/Assets/Scripts/Progression/TriggerEnd.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnd : MonoBehaviour
+{
+    [Tooltip("Game object to be enabled when all sound objects are found")]
+    [SerializeField] private GameObject toEnable;
+
+    private Count c;
+    private bool notExecuted = true;
+    // Start is called before the first frame update
+    void Start(){
+        c = GameObject.Find("Count").GetComponent<Count>();
+        print(c);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(notExecuted && c.AllCounted()){
+            toEnable.SetActive(true);
+            notExecuted = false;
+        }
+    }
+}
