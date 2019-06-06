808507fed13c900960a9fb2eb8d15813bbc66247
jwynd <18224844+jwynd@users.noreply.github.com>
2019-06-06 13:25:03 -0700

    Cursor locked when not in use

---
+++ HiddenHarmony/Assets/Scripts/CursorLock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour
+{
+    void Update()
+    {
+        if(Cursor.visible){
+            Cursor.lockState = CursorLockMode.None;
+        } else {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
