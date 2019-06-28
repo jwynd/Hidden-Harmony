fab26eae0c7d9470069bdf6a9b8a42d9ddc4c886
jwynd <18224844+jwynd@users.noreply.github.com>
2019-06-28 12:26:45 -0700

    Created billboard script for sprites

---
+++ HiddenHarmony/Assets/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Billboard : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 lookTarget;
+    // Start is called before the first frame update
+    void Awake(){
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        lookTarget = player.position;
+        lookTarget.y = this.transform.position.y;
+        this.transform.LookAt(lookTarget);
+    }
+}
