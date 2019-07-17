455399b0296a27cafc5353c03379d0fed4d7aff1
BOOG\Jenny <jfullert@ucsc.edu>
2019-07-17 12:57:51 -0700

    music visualizer basic setup

---
+++ HiddenHarmony/Assets/Scripts/MusicVisController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicVisController : MonoBehaviour
+{
+    [SerializeField] private Material low;
+    [SerializeField] private Material mid;
+    [SerializeField] private Material hi;
+    [SerializeField] private float spectrumScalar = 10;
+
+    private float[] buckets = new float[3];
+    [SerializeField] private int[] cutoffs = new int[3];
+
+    void Start()
+    {
+        for(int i = 0; i<buckets.Length; i++){
+            buckets[i] = 0;
+        }
+    }
+
+    void Update()
+    {
+    
+        for(int i = 0; i<buckets.Length; i++){
+            // reset the bucket
+            buckets[i] = 0;
+
+            // go through this bucket's section of cutoff data
+            //  and sum it
+            for(int j = i==0?0:cutoffs[i-1]; j<cutoffs[i]; j++){
+                buckets[i] += W_AudioPeer.spectrumData[j];
+            }
+
+            // make sure it is in range of 0-1
+            Mathf.Clamp(buckets[i], 0, 1);
+        }
+
+        // use the informatin from the buckets to lerp material colors
+        // lerp low
+        Color lerpCol = Color.Lerp(Color.black, Color.red, buckets[0]);
+        low.SetColor("_Color", lerpCol);
+
+        // lerp mid
+        lerpCol = Color.Lerp(Color.black, Color.green, buckets[1]);
+        mid.SetColor("_Color", lerpCol);
+
+        // lerp hi
+        lerpCol = Color.Lerp(Color.black, Color.blue, buckets[2]);
+        hi.SetColor("_Color", lerpCol);
+    }
+}
---
+++ HiddenHarmony/Assets/Scripts/W_AudioPeer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// create this required component....
+public class W_AudioPeer : MonoBehaviour {
+
+    // NOTE: make this a 'static' float so we can access it from any other script.
+    public static float[] spectrumData = new float[512];
+
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        GetSpectrumAudioSource ();
+
+    }
+
+
+    void GetSpectrumAudioSource()
+    {
+        // this method computes the fft of the audio data, and then populates spectrumData with the spectrum data.
+        AudioListener.GetSpectrumData (spectrumData, 0, FFTWindow.Hanning);
+    }
+}
+
+
