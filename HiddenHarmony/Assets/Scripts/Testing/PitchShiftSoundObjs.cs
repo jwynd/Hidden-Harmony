9767aa2c4be24915df24cf90fbea8e20b954222c
Kiefer Nemeth <kiefernemeth@gmail.com>
2019-08-22 12:04:50 -0700

    Added pitch shifting script for soundobjs

---
+++ HiddenHarmony/Assets/Scripts/Testing/PitchShiftSoundObjs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PitchShiftSoundObjs : MonoBehaviour
+{
+    public AudioMixer soundObjPitch;
+    public float pitch; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        soundObjPitch.SetFloat("SoundObjPitch", pitch);
+    }
+}
