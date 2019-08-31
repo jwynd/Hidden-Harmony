1cc2e8ca11f0ff6bfdeae279edb8f44d18a0c057
jswanso1 <38990292+jswanso1@users.noreply.github.com>
2019-08-31 12:27:12 -0700

    Bouncy script
    Attach to anything bouncy

---
+++ HiddenHarmony/Assets/Scripts/Bouncy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncy : MonoBehaviour
+{
+    [Tooltip("How high the player bounces (Must be greater than the terminalVelocity variable of the player)")]
+    public float bounceFactor = 70; //must be higher than termialVelocity of the player
+    private Transform player;
+    private RaycastHit hit;
+    [SerializeField] private float bounceDetectionDistance = 3f;
+
+    void Start(){
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update(){
+        if(Physics.Raycast(player.position, Vector3.down, out hit, bounceDetectionDistance)){
+            if(hit.collider.gameObject == this.gameObject){
+                player.gameObject.GetComponent<PlayerMovement>().yVelocity = bounceFactor;
+            }
+        }
+    }
+}
