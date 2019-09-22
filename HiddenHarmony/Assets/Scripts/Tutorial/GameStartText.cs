d6e7f2b91462cecfa9008422ff89fb2900034ce3
Kiefer Nemeth <kiefernemeth@gmail.com>
2019-09-21 17:22:22 -0700

    Modified GameSTartText.cs

--- HiddenHarmony/Assets/Scripts/Tutorial/GameStartText.cs
+++ HiddenHarmony/Assets/Scripts/Tutorial/GameStartText.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GameStartText : MonoBehaviour
 {
+    private ComposeModeTransition cmt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cmt = GameObject.Find("GameplayObjects/CameraChange").GetComponent<ComposeModeTransition>();
+        // Disable Tab until player has spoken with Chad directly
+        cmt.setTransition(false);
+
+
+        // Destroy self upon completion of tasks
+        Destroy(this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
