a5ccf5f7edea18f232653658ba312a0c7bc7540d
Kiefer Nemeth <kiefernemeth@gmail.com>
2019-11-08 16:35:32 -0800

    Added initial Chad Dialogue

---
+++ HiddenHarmony/Assets/Scripts/Tutorial/ChadDialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChadDialogue : MonoBehaviour
+{
+    // This is a collection of functions to be triggered by Chad's 
+    // text boxes. 
+
+    public void DestroyCurrentText()
+    {
+        // For use when the current text box is a "one-time use" sort of deal
+        Destroy(this.transform.parent.GetChild(0).gameObject);
+    }
+}
