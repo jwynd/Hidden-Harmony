4c6497cbabe4c78a148b90b6f91e6cdcc1f44a1b
Kiefer Nemeth <kiefernemeth@gmail.com>
2019-11-15 19:58:50 -0800

    Beeg progress on tutorial. not done

---
+++ HiddenHarmony/Assets/Scripts/Tutorial/StageTutorial.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageTutorial : MonoBehaviour
+{
+    // When player performs actions in compose mode, create/destroy tutorial images
+    // on the canvas
+
+    // This list of slices may expand indefinitely:
+    // dependent on how many steps there are in the tutorial
+    // slice ex: "HighlightInventory" is a circle around the player's inv UI
+    [SerializeField] private GameObject slice1; // PressTab
+    [SerializeField] private GameObject slice2; // ClickFirstStage
+    [SerializeField] private GameObject slice3; // AddFirstSound
+    [SerializeField] private GameObject slice4; // AddSecondSound
+    [SerializeField] private GameObject slice5;
+
+    [SerializeField] private GameObject stagesParent;
+    [SerializeField] private ComposeModeTransition cmt;
+
+    private GameObject firstStage;
+    private GameObject secondStage;
+    private Transform[] stages = new Transform[9];
+
+    private bool firstCompose = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cmt = GameObject.Find("GameplayObjects/CameraChange").GetComponent<ComposeModeTransition>();
+        for(int n = 0; n < stagesParent.transform.childCount; n++)
+        {
+            stages[n] = stagesParent.transform.GetChild(n);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(firstCompose == false && cmt.Compose())
+        {
+            firstCompose = true;
+            slice1.SetActive(false); // "Press Tab to Compose" text
+            slice2.SetActive(true); // 'click on stage' prompt
+        } else if(firstCompose && cmt.Compose() == false)
+        {
+            // Done with the tutorial sequence
+            slice4.SetActive(false);
+        }
+        if(firstStage == null)
+        {
+            // Loop through (actual) stages until one becomes active
+            // This event signifies the player activating a stage, triggering new dialogue
+            foreach(Transform s in stages)
+            {
+                if (s.gameObject.activeSelf)
+                {
+                    print("first stage activated");
+                    firstStage = s.gameObject;
+                    slice2.SetActive(false); // 'click on stage' prompt
+                    slice3.SetActive(true); // AddFirstSound
+                }
+            }
+        } else if(secondStage == null)
+        {
+            // Check for first stage usage
+            if (firstCompose == false && firstStage.GetComponent<Stage>().IsOccupied())
+            {
+                print("firstStage used");
+                firstCompose = true;
+                slice3.SetActive(false);
+                slice4.SetActive(true);
+            }
+
+            // Check for new second stage
+            foreach (Transform s in stages)
+            {
+                if (s.gameObject.activeSelf && s.gameObject != firstStage)
+                {
+                    print("second stage activated");
+                    secondStage = s.gameObject;
+                    slice3.SetActive(false);
+                    slice4.SetActive(true);
+                }
+            }
+        }
+    }
+}
