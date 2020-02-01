c93e52fa0ed78340ea1212484c8bfca733dc1fc2
Kiefer Nemeth <kiefernemeth@gmail.com>
2020-01-31 17:05:22 -0800

    Added GodMode script

---
+++ HiddenHarmony/Assets/Scripts/Dev Tools/GodMode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodMode : MonoBehaviour
+{
+    private GameObject player;
+    private bool active; // whether God Mode is currently active or not
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+    }
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && Input.GetKeyDown(KeyCode.G))
+        {  
+            // Toggle God Mode
+            active = !active;
+        }
+
+        if(active)
+        {
+            // Enable God mode things!
+        }
+    }
+}
