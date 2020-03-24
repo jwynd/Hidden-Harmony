96a9294d0b4905d62b0bc137e51065470013eaea
Kiefer Nemeth <kiefernemeth@gmail.com>
2020-03-24 16:19:05 -0700

    Added ring puzzle to Hub

--- HiddenHarmony/Assets/Scripts/Puzzles/Ring.cs
+++ HiddenHarmony/Assets/Scripts/Puzzles/Ring.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class Ring : MonoBehaviour
 {
-    public RingPuzzle parent; 
+    public RingPuzzle controller;
+
+    private ParticleSystem burst;
+    private GameObject sprite;
+    private bool activated = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        burst = gameObject.GetComponent<ParticleSystem>();
+        sprite = transform.parent.Find("Sprite").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(activated)
+        {
+            if(burst.isStopped)
+            {
+                transform.parent.gameObject.SetActive(false);
+                sprite.SetActive(true); // Reenable sprite renderer (even though the parent is disabled)
+            }
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        // Collect this ring.
-        // Send collection to RingPuzzle.cs and disable this object
-        parent.Collect();
-        gameObject.SetActive(false);
+        if(activated == false)
+        {
+            // Collect this ring.
+            print("Colliding with Ring");
+            controller.Collect(); // Send data to RingPuzzle.cs
+            sprite.SetActive(false); // Disable sprite renderer
+            burst.Play(); // Object will disable once particles are finished
+            activated = true;
+        }
     }
 }
--- HiddenHarmony/Assets/Scripts/Puzzles/RingPuzzle.cs
+++ HiddenHarmony/Assets/Scripts/Puzzles/RingPuzzle.cs
@@ -5,51 +5,74 @@ using UnityEngine;
 public class RingPuzzle : MonoBehaviour
 {
     public GameObject reward;
+    public int timerDuration = 10;
 
+    private bool activated = false;
     private int collected = 0;
-    private float timer = 10f;
+    private float timer;
     private int total;
+    private AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
         total = transform.childCount;
         reward.SetActive(false);
+        audio = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Count down timer. When it hits 0, reset puzzle
-        timer -= Time.deltaTime;
-        if(timer < 0)
+        if(activated)
         {
-            Stop();
+            // Play a ticking sound every second
+            if (Mathf.Approximately(timer, Mathf.RoundToInt(timer)))
+            {
+                // Timer is a whole number (integer)
+                // Play sound!
+                print("Ring Timer: " + timer);
+                audio.Play();
+            }
+            // Count down timer. When it hits 0, reset puzzle
+            timer -= Time.deltaTime;
+
+            if (timer < 0)
+            {
+                // Player ran out of time!
+                Stop();
+            }
         }
     }
 
     public void Initiate()
     {
         // Starts puzzle, enables Ring objects
-        foreach(GameObject ring in gameObject.GetComponentsInChildren<GameObject>())
+        print("Ring Puzzle Initiated");
+        timer = (float)timerDuration;
+        for(int i = 0; i < transform.childCount; i++)
         {
-            ring.SetActive(true);
+            transform.GetChild(i).gameObject.SetActive(true);
         }
+        activated = true;
     }
 
     private void Stop()
     {
         // Stops puzzle, either when the player wins or the timer runs out
+        print("Ring Puzzle Stopped");
         collected = 0;
-        foreach (GameObject ring in gameObject.GetComponentsInChildren<GameObject>())
+        for (int i = 0; i < transform.childCount; i++)
         {
-            ring.SetActive(false);
+            transform.GetChild(i).gameObject.SetActive(false);
         }
+        activated = false;
     }
 
     public void Collect()
     {
         // Called by Ring.cs when player enters its collider
+        print("Ring Collected");
         collected++;
         if(collected >= total)
         {
