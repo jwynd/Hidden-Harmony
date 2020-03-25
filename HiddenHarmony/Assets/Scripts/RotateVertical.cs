0c715c17d6cadcf6a206ed421af857e8936c2de5
Kiefer Nemeth <kiefernemeth@gmail.com>
2020-03-24 17:17:06 -0700

    Updated Hub ring puzzle

---
+++ HiddenHarmony/Assets/Scripts/RotateVertical.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateVertical : MonoBehaviour
+{
+    // Rotates along a horizontal axis, making the object appear to rotate vertically
+    public float speed = 70f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+}
