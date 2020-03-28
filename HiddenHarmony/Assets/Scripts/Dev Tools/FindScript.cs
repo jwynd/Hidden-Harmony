fc58e7814761bb1927631ee3bdffb44b7951e547
Kiefer Nemeth <kiefernemeth@gmail.com>
2020-03-28 14:54:05 -0700

    Added FindScript.cs

---
+++ HiddenHarmony/Assets/Scripts/Dev Tools/FindScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FindScript : MonoBehaviour
+{
+    [Header("Enter the name of the class you want to search for")]
+    public string scriptName;
+
+    private UnityEngine.Object[] objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Type type = Type.GetType(scriptName);
+        objects = FindObjectsOfType(Type.GetType(scriptName));
+        print(objects);
+        print(objects[0]);
+        foreach(UnityEngine.Object obj in objects)
+        {
+            print(obj.name);
+        }
+    }
+}
