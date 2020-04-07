9ea5dfa6cd94ea3c530ce18238df6ab953a0c5b8
jwynd <18224844+jwynd@users.noreply.github.com>
2020-04-07 12:56:54 -0700

    Added disable or enable script

---
+++ HiddenHarmony/Assets/Scripts/DisableOrEnable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOrEnable : MonoBehaviour
+{
+    public enum Behave
+    {
+        Enable = 0,
+        Disable = 1
+    }
+    [Tooltip("Determine if this script will disable or enable the listed game objects on trigger enter")]
+    public Behave enableOrDisable = Behave.Enable;
+    public GameObject[] gameObjects;
+    private GameObject player;
+    
+    void Awake(){
+        player = GameObject.Find("Player");
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject == player){
+            foreach(GameObject g in gameObjects){
+                g.SetActive(enableOrDisable == Behave.Enable);
+            }
+        }
+    }
+}
