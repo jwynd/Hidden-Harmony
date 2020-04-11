926adde2a44b059d4269ae4f3fab563bad802007
jwynd <18224844+jwynd@users.noreply.github.com>
2020-04-10 20:24:36 -0700

    ListTag now lists all objects regarless of if they are enabled, and will indicate if a given object is active or inactive

--- HiddenHarmony/Assets/Scripts/ListTag.cs
+++ HiddenHarmony/Assets/Scripts/ListTag.cs
@@ -13,27 +13,33 @@ public class ListTag : MonoBehaviour
             Destroy(this);
         }
         Instance = this;
-        ListActiveTag(tag);
+        listTag(tag);
     }
 
-    private string GetPath(GameObject g){
+    private string GetPath(GameObject g, out bool active){
         string path = "/" + g.name;
+        active = g.activeSelf;
         while (g.transform.parent != null)
         {
             g = g.transform.parent.gameObject;
+            if(g.activeSelf == false) active = false;
             path = "/" + g.name + path;
         }
         return path;
     }
 
-    public void ListActiveTag(string t){
-        GameObject[] objs = GameObject.FindGameObjectsWithTag(t);
+    public void listTag(string t){
+        GameObject[] objs = Resources.FindObjectsOfTypeAll<GameObject>();
         int count = 0;
-        Debug.Log("BEGIN SEARCH FOR ACTIVE OBJECTS WITH TAG \"" + t +"\"");
+        Debug.Log("BEGIN SEARCH FOR OBJECTS WITH TAG \"" + t +"\"");
         foreach(GameObject o in objs){
-            Debug.Log("LISTED OBJECT WITH TAG \"" + t +"\": " + GetPath(o));
-            count++;
+            if(o.tag == t){
+                bool a = true;
+                string path = GetPath(o, out a);
+                Debug.Log((a?"ACTIVE":"INACTIVE") + " OBJECT WITH TAG \"" + t +"\": " + path);
+                count++;
+            }
         }
-        Debug.Log("END SEARCH. FOUND " + count + " ACTIVE SOUND OBJECTS WITH TAG \""+t+"\"");
+        Debug.Log("END SEARCH. FOUND " + count + " OBJECTS WITH TAG \""+t+"\"");
     }
 }
