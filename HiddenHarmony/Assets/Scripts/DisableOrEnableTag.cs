f5310e433a0ad1cb8c399377576dfa0301e8fbef
jwynd <18224844+jwynd@users.noreply.github.com>
2020-04-10 19:53:05 -0700

    Created List Tag as well as DisableOrEnableTag

---
+++ HiddenHarmony/Assets/Scripts/DisableOrEnableTag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOrEnableTag : MonoBehaviour
+{
+    public enum Behavior
+    {
+        Enable = 0,
+        Disable = 1
+    }
+    [Tooltip("Determine if this script will disable or enable the listed game objects on trigger enter")]
+    public Behavior enableOrDisable = Behavior.Enable;
+    public string[] tags;
+    private List<List<GameObject>> gameObjects = new List<List<GameObject>>();
+    private GameObject player;
+    
+    void Awake(){
+        player = GameObject.Find("Player");
+        foreach(string t in tags){
+            gameObjects.Add(PopulateList(t));
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        foreach(List<GameObject> l in gameObjects){
+            foreach(GameObject g in l){
+                g.SetActive(enableOrDisable == Behavior.Enable);
+            }
+        }
+    }
+
+    private List<GameObject> PopulateList(string t){
+        GameObject[] gs = Resources.FindObjectsOfTypeAll<GameObject>();
+        List<GameObject> rgs = new List<GameObject>();
+        foreach(GameObject g in gs){
+            if(g.tag == t) rgs.Add(g);
+        }
+        return rgs;
+    }
+}
---
+++ HiddenHarmony/Assets/Scripts/ListTag.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListTag : MonoBehaviour
+{
+    public static ListTag Instance;
+    public string tag = "Untagged";
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(Instance != null && Instance != this){
+            Destroy(this);
+        }
+        Instance = this;
+        ListActiveTag(tag);
+    }
+
+    private string GetPath(GameObject g){
+        string path = "/" + g.name;
+        while (g.transform.parent != null)
+        {
+            g = g.transform.parent.gameObject;
+            path = "/" + g.name + path;
+        }
+        return path;
+    }
+
+    public void ListActiveTag(string t){
+        GameObject[] objs = GameObject.FindGameObjectsWithTag(t);
+        int count = 0;
+        Debug.Log("BEGIN SEARCH FOR ACTIVE OBJECTS WITH TAG \"" + t +"\"");
+        foreach(GameObject o in objs){
+            Debug.Log("LISTED OBJECT WITH TAG \"" + t +"\": " + GetPath(o));
+            count++;
+        }
+        Debug.Log("END SEARCH. FOUND " + count + " ACTIVE SOUND OBJECTS WITH TAG \""+t+"\"");
+    }
+}
