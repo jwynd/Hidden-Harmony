272099b1cd44c0af1ceba775bbc7c445020bb4eb
Kiefer Nemeth <kiefernemeth@gmail.com>
2020-04-14 17:24:36 -0700

    First pass at SimonPuzzle scripts/objects

---
+++ HiddenHarmony/Assets/Scripts/Puzzles/SimonObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonObject : MonoBehaviour
+{
+    public SimonPuzzle controller;
+    public int note;
+
+    private RaycastHit hit;
+    private Ray ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Input.GetMouseButton(0) && Physics.Raycast(ray, out hit, 11.0f) && hit.transform == transform)
+        {
+            controller.CheckNote(note);
+        }
+    }
+}
---
+++ HiddenHarmony/Assets/Scripts/Puzzles/SimonPuzzle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonPuzzle : MonoBehaviour
+{
+    public int[] pattern;
+    public GameObject reward;
+
+    private int current = 0;
+
+    private AudioSource victorySFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reward.SetActive(false);
+        victorySFX = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckNote(int note)
+    {
+        if(pattern[current] == note)
+        {
+            current++;
+            if(current >= pattern.Length-1)
+            {
+                // You did it!
+                victorySFX.Play();
+                reward.SetActive(true);
+            }
+        }
+    }
+}
