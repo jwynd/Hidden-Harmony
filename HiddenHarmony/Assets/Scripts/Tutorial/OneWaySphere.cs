4b886e1196ebf8cf7c8bc3a413413e660a75732f
Kiefer Nemeth <kiefernemeth@gmail.com>
2020-04-26 15:44:50 -0700

    Added containment spherein Hub

---
+++ HiddenHarmony/Assets/Scripts/Tutorial/OneWaySphere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class OneWaySphere : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+        // Invert Mesh
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+
+        gameObject.AddComponent<MeshCollider>();
+    }
+}
