535c73be0c333bd0d23c747247b5baf1900205b4
Kiefer Nemeth <kiefernemeth@gmail.com>
2020-05-17 16:32:19 -0700

    Added a respawn point for if player goes below value -300

---
+++ HiddenHarmony/Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private GameObject player;
+    private Transform respawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawn = GameObject.Find("GameplayObjects/RespawnPoint").transform;
+    }
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.y < -300f)
+        {
+            player.transform.position = respawn.position;
+        }
+    }
+}
