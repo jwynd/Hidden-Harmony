100fe3e6d35c5106462c7b18260bd843d81cd0b8
Kiefer Nemeth <kiefernemeth@gmail.com>
2020-05-26 16:04:08 -0700

    First draft of script that changes which Chad is present

---
+++ HiddenHarmony/Assets/Scripts/ChadChange.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChadChange : MonoBehaviour
+{
+    private Count count;
+
+    public int chadCopy = 0; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = GameObject.Find("GameplayObjects/Count").GetComponent<Count>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Switch Chad copies based on the following criteria
+        if (chadCopy < 6)
+        {
+            if (count.AllCounted())
+            {
+                // All Artifacts have been collected & used
+                GameObject.Find("Hub/--AREA-PLATFORMS--/ChadAlive6").SetActive(true);
+                gameObject.SetActive(false);
+            }
+            else if (count.statuesUsed == 3)
+            {
+                // Areas returned, but more Artifacts to collect
+                GameObject.Find("Hub/--AREA-PLATFORMS--/ChadAlive5").SetActive(true);
+                gameObject.SetActive(false);
+            }
+            else if (count.statuesUsed > 0)
+            {
+                // At least 1 area has been returned
+                GameObject.Find("Hub/--AREA-PLATFORMS--/ChadAlive4").SetActive(true);
+                gameObject.SetActive(false);
+            }
+            else if (count.DenCount() >= 3 || count.ForestCount() >= 3 || count.CavernCount() >= 3)
+            {
+                GameObject.Find("Hub/--AREA-PLATFORMS--/ChadAlive3").SetActive(true);
+                gameObject.SetActive(false);
+            }
+            else if (count.DenCount() >= 1 || count.ForestCount() >= 1 || count.CavernCount() >= 1)
+            {
+                GameObject.Find("Hub/--AREA-PLATFORMS--/ChadAlive2").SetActive(true);
+                gameObject.SetActive(false);
+            }
+        } 
+
+    }
+}
